bfb9dce5d2312a8c6f414a2fd232ead1c115ebd7
Dmytro Nedvyha <nedvigadimon@gmail.com>
2020-03-19 02:15:31 +0200

    eleanor-soft

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true
+                }
+            }
+        ]
+    })
+});
